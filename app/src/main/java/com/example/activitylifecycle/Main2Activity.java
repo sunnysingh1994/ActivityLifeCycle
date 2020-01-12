1955b03fb79f02072d881c3dab3685e6a5e9f1d3
sunnysingh1994 <sunnysingh1994.ss@gmail.com>
2020-01-12 14:49:40 +0530

    Final Changes

--- app/src/main/java/com/example/activitylifecycle/Main2Activity.java
+++ app/src/main/java/com/example/activitylifecycle/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,4 +12,40 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy2", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart2", Toast.LENGTH_SHORT).show();
+    }
+
 }
